4e2e8b724f653c686bb293e0123dfc8de3c7f812
rroyermoraes <rroyermoraes@outlook.com>
2018-08-11 04:08:12 -0300

    NavmeshTests

---
+++ TCC_Mark_Roger/Assets/Frog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Frog{
+
+    public enum FrogColor
+    {
+        Azul,
+        Verde,
+        Vermelho,
+        Amarelo,
+        Branco,
+        Preto
+    };
+
+    public enum Pattern
+    {
+        ListrasVerticais,
+        ListrasHorizontais,
+        Manchas,
+        Pintas
+    };
+
+    [Header("Frog properties")]
+    public FrogColor color1;
+    public Pattern pattern;
+    public FrogColor color2;
+
+    public Frog() {
+        color1 = FrogColor.Azul;
+        pattern = Pattern.Manchas;
+        color2 = FrogColor.Branco;
+
+    }
+}
---
+++ TCC_Mark_Roger/Assets/FrogGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogGame : MonoBehaviour {
+
+    
+    public Frog targetFrog = new Frog();
+
+    public bool CompareFrogs(Frog b) {
+        if(b.color1==targetFrog.color1 && b.pattern == targetFrog.pattern && b.color2 == targetFrog.color2)
+        {
+            return true;
+        }
+        return false;
+    }
+}
