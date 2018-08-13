b05763cedee19aaf7f73593e28e16e947a50032f
rroyermoraes <rroyermoraes@outlook.com>
2018-08-12 21:05:51 -0300

    refinamento navmesh

---
+++ TCC_Mark_Roger/Assets/FollowXYPosition.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FollowXYPosition : MonoBehaviour {
+
+    public Transform target;
+	
+	void Update () {
+        transform.position = new Vector3(target.position.x,target.position.y,0f);
+
+	}
+}
