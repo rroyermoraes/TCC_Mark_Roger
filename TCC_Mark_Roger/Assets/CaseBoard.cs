c7df97ab88c28021071afa9f9843f92325d73b72
rroyermoraes <rroyermoraes@outlook.com>
2018-08-30 01:30:44 -0300

    Testes Retratos de NPC iniciais

--- TCC_Mark_Roger/Assets/CaseBoard.cs
+++ TCC_Mark_Roger/Assets/CaseBoard.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CaseBoard : MonoBehaviour {
 
-
+    public GameObject blankNode;
     public List<CaseNode> nodes = new List<CaseNode>();
     public List<CaseLink> links = new List<CaseLink>();
 
@@ -15,8 +15,16 @@ public class CaseBoard : MonoBehaviour {
                 return false;
             }
         }
+        if (nl.a == nl.b) {
+            Destroy(nl.gameObject);
+            return false;
+        }
         links.Add(nl);
         return true;
+    }
+    public void AddBlank() {
+        GameObject g = Instantiate(blankNode,this.transform);
+        nodes.Add(g.GetComponent<CaseNode>());
     }
 
     public void AddItemToBoard(CaseNode cn) {
---
+++ TCC_Mark_Roger/Assets/RightClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class RightClick : MonoBehaviour, IPointerClickHandler
+{
+
+    public UnityEvent rightClick;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+            rightClick.Invoke();
+    }
+}
