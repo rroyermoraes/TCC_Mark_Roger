5d4839022b10b4b67488cf0fddf93f088a5576a2
rroyermoraes <rroyermoraes@outlook.com>
2018-09-04 20:52:08 -0300

    TestController

--- TCC_Mark_Roger/Assets/Scipts/General/PlayerController.cs
+++ TCC_Mark_Roger/Assets/Scipts/General/PlayerController.cs
@@ -2,25 +2,136 @@
 using UnityEngine.AI;
 using UnityEngine.EventSystems;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public Camera cam;
+    public Animator anim;
+    public float stopAnimBias = 0;
     public NavMeshAgent agent;
-	
-	// Update is called once per frame
-	void Update () {
+    float angle;
+    // Update is called once per frame
+    void Update()
+    {
         if ((EventSystem.current.IsPointerOverGameObject()) || (ContextMenuContainer.contextMenuActive))
         {
             return;
         }
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
 
-            Ray ray =cam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit)) {
+            if (Physics.Raycast(ray, out hit))
+            {
                 //move agent
                 agent.SetDestination(hit.point);
-            }                       
+            }
         }
-	}
+        
+        angle = Mathf.Atan2(agent.velocity.y, agent.velocity.x)*Mathf.Rad2Deg;
+
+        if (agent.velocity.magnitude > stopAnimBias)
+        {
+            if ((angle >= 0 && angle <= 45)||(angle <= 0 && angle >= -45))
+            {
+                anim.SetBool("Right", true);
+
+                anim.SetBool("Left", false);
+                anim.SetBool("Up", false);
+                anim.SetBool("Down", false);
+            }
+            if (angle > 45 && angle < 135)
+            {
+                anim.SetBool("Up", true);
+
+                anim.SetBool("Down", false);
+                anim.SetBool("Right", false);
+                anim.SetBool("Left", false);
+            }
+            if ((angle >= 135 && angle <= 180)||(angle <= -135 && angle >= -180))
+            {
+                anim.SetBool("Left", true);
+
+                anim.SetBool("Up", false);
+                anim.SetBool("Down", false);
+                anim.SetBool("Right", false);
+            }
+            if (angle < -45 && angle > -135)
+            {
+                anim.SetBool("Down", true);
+
+                anim.SetBool("Up", false);
+                anim.SetBool("Right", false);
+                anim.SetBool("Left", false);
+            }
+
+
+        }
+        else {
+            anim.SetBool("Left", false);
+            anim.SetBool("Right", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Down", false);
+        }
+    
+    
+        
+
+        /*
+
+        if (agent.velocity.magnitude > stopAnimBias)
+        {
+            if (Mathf.Abs(agent.velocity.x) > Mathf.Abs(agent.velocity.y / 2))
+            {
+                if (agent.velocity.x > 0)
+                {
+                    anim.SetBool("Right", true);
+
+                    anim.SetBool("Left", false);
+                    anim.SetBool("Up", false);
+                    anim.SetBool("Down", false);
+                }
+                if (agent.velocity.x < 0)
+                {
+                    anim.SetBool("Left", true);
+
+                    anim.SetBool("Up", false);
+                    anim.SetBool("Down", false);
+                    anim.SetBool("Right", false);
+                }
+
+            }
+            else
+            {
+                if (agent.velocity.y > 0)
+                {
+                    anim.SetBool("Up", true);
+
+                    anim.SetBool("Down", false);
+                    anim.SetBool("Right", false);
+                    anim.SetBool("Left", false);
+                }
+                if (agent.velocity.y < 0)
+                {
+                    anim.SetBool("Down", true);
+
+                    anim.SetBool("Up", false);
+                    anim.SetBool("Right", false);
+                    anim.SetBool("Left", false);
+                }
+
+            }
+
+
+        }
+        else
+        {
+            anim.SetBool("Down", false);
+            anim.SetBool("Up", false);
+            anim.SetBool("Right", false);
+            anim.SetBool("Left", false);
+
+        }*/
+    }
 }
