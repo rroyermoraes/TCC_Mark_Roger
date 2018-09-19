b69965568e25f449358ac75c9d5581dcb3cbc87b
rroyermoraes <rroyermoraes@outlook.com>
2018-09-18 22:42:21 -0300

    Aprimoramento Desafio1

--- TCC_Mark_Roger/Assets/Scipts/Desafio 1/DesafioBebida.cs
+++ TCC_Mark_Roger/Assets/Scipts/Desafio 1/DesafioBebida.cs
@@ -29,6 +29,15 @@ public class DesafioBebida : MonoBehaviour {
     public SliderAnimator b1Slider;
     public SliderAnimator b2Slider;
     public SliderAnimator b3Slider;
+    public SliderAnimator b4Slider;
+
+    public SliderAnimator a1Slider;
+    public SliderAnimator a2Slider;
+    public SliderAnimator a3Slider;
+
+    public Animator d1Animator;
+    public Animator d2Animator;
+    public Animator d3Animator;
 
 
 
@@ -45,43 +54,91 @@ public class DesafioBebida : MonoBehaviour {
 
 
     //Turn Preparations
-    private void SetA(List<int> nA)
+    IEnumerator SetA(List<int> nA)
     {
-        //a = nA;
+        EnableDisableExchange(false);
+        a1Slider.AnimateSlide(0);
+        a2Slider.AnimateSlide(0);
+        a3Slider.AnimateSlide(0);
+        yield return new WaitForSeconds(2);
+        
+
         for (int j = 0; j < 4; j++)
         {
             a[j] = nA[j];
+            bDrink[j].value = 0;
 
         }
+
+        //fill
+        b1Slider.AnimateSlide(a[3]);
+        yield return new WaitForSeconds(1);
+        b2Slider.AnimateSlide(a[0]);
+        yield return new WaitForSeconds(1);
+        b3Slider.AnimateSlide(a[1]);
+        yield return new WaitForSeconds(1);
+        b4Slider.AnimateSlide(a[2]);
+        yield return new WaitForSeconds(1);
+        //pause
+        yield return new WaitForSeconds(1.5f);
+        //drink
+        d1Animator.SetBool("Drink", true);
+        d2Animator.SetBool("Drink", true);
+        d3Animator.SetBool("Drink", true);
+        b1Slider.AnimateSlide(0);
+        a1Slider.AnimateSlide(a[3]);
+        b2Slider.AnimateSlide(0);
+        a2Slider.AnimateSlide(a[0]);
+        b3Slider.AnimateSlide(0);
+        a3Slider.AnimateSlide(a[1]);
+        b4Slider.AnimateSlide(0);
+
+
+        //wait
+        yield return new WaitForSeconds(1);
         for (int i = 0; i < 4; i++)
         {
             aF[i].text = a[i].ToString();
 
         }
-       // b1Slider.value = a[3];
-        b1Slider.AnimateSlide(a[3]);
-        // b2Slider.value = a[0];
-        b2Slider.AnimateSlide(a[0]);
-        //  b3Slider.value = a[1];
-        b3Slider.AnimateSlide(a[1]);
+        d1Animator.SetBool("Drink", false);
+        d2Animator.SetBool("Drink", false);
+        d3Animator.SetBool("Drink", false);
+        SetTurnB();
+
     }
 
-    private void SetB(List<int> nB)
+    IEnumerator SetB(List<int> nB)
     {
+        yield return new WaitForSeconds(2);
         //b = nB;
         for (int j = 0; j < 4; j++)
         {
             b[j] = nB[j];
 
         }
+
+        b1Slider.AnimateSlide(b[3]);
+        yield return new WaitForSeconds(1);
+        b2Slider.AnimateSlide(b[0]);
+        yield return new WaitForSeconds(1);
+        b3Slider.AnimateSlide(b[1]);
+        yield return new WaitForSeconds(1);
+        b4Slider.AnimateSlide(b[2]);
+        yield return new WaitForSeconds(1);
+
+
+
+
+
         for (int i = 0; i < 4; i++)
         {
             bF[i].text = b[i].ToString();
-            bDrink[i].value = b[i];
+            //bDrink[i].value = b[i];
 
         }
-
-}
+        EnableDisableExchange(true);
+    }
 
     private void SetMaxMoves(int i)
     {
@@ -110,6 +167,13 @@ public class DesafioBebida : MonoBehaviour {
     private void CommitTurn()
     {
         bool result = true;
+        b1Slider.AnimateSlide(0);
+
+        b2Slider.AnimateSlide(0);
+
+        b3Slider.AnimateSlide(0);
+
+        b4Slider.AnimateSlide(0);
         List<int> c = new List<int>(new int[4]);
         EnableDisableExchange(false);
         for (int i = 0; i < 4; i++) {
@@ -124,24 +188,38 @@ public class DesafioBebida : MonoBehaviour {
         {
             cF[i].text = c[i].ToString();
             if (c[3] <= 10){
-              //  b1Slider.value = c[3];
+               
             }
             if (c[0] <= 10)
             {
-               // b2Slider.value = c[0];
+               
             }
             if (c[1] <= 10)
             {
-               // b3Slider.value = c[1];
+               
             }
 
         }
+        StartCoroutine(LastDrink(c));
 
 
 
         CheckTurn(result);
     }
 
+    IEnumerator LastDrink(List<int> c){
+        d1Animator.SetBool("Drink", true);
+        d2Animator.SetBool("Drink", true);
+        d3Animator.SetBool("Drink", true);
+        a1Slider.AnimateSlide(c[3]);
+        a2Slider.AnimateSlide(c[0]);
+        a3Slider.AnimateSlide(c[1]);
+        yield return new WaitForSeconds(1);
+        d1Animator.SetBool("Drink", false);
+        d2Animator.SetBool("Drink", false);
+        d3Animator.SetBool("Drink", false);
+
+    }
 
 
     public void ConfirmButton() {
@@ -151,10 +229,11 @@ public class DesafioBebida : MonoBehaviour {
     }
     private void SetTurnA()
     {
-        SetA(turnos[activeTurn].iA);
+        
+        //StartCoroutine(SetA(turnos[activeTurn].iA));
         for (int i = 0; i < 4; i++)
         {
-            bF[i].text = "0";
+            bF[i].text = " ";
 
         }
         SetMaxMoves(turnos[activeTurn].iMaxMoves);
@@ -162,16 +241,16 @@ public class DesafioBebida : MonoBehaviour {
         moves = 0;
         moveCountF.text = moves.ToString();
 
-
-        SetTurnB();
+        StartCoroutine(SetA(turnos[activeTurn].iA));
+        
 
     }
 
     private void SetTurnB()
     {
+        StartCoroutine(SetB(turnos[activeTurn].iB));
 
-        SetB(turnos[activeTurn].iB);
-        EnableDisableExchange(true);
+        
 
     }
 
