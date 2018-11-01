7af3e176c769c3540834b217bfc70874e8acf2d2
rroyermoraes <rroyermoraes@outlook.com>
2018-11-01 01:48:31 -0300

    Desafio 1 mudancas

--- TCC_Mark_Roger/Assets/SliderAnimator.cs
+++ TCC_Mark_Roger/Assets/SliderAnimator.cs
@@ -6,31 +6,68 @@ using UnityEngine.UI;
 public class SliderAnimator : MonoBehaviour {
 
     public float speed=2f;
+    public bool fadeSlide = false;
+    private Slider sl;
 
     public void AnimateSlide(float v)
     {
-
-        StartCoroutine(Slide(v));
+        sl = GetComponent<Slider>();
+        if (fadeSlide) {
+            StartCoroutine(FadeSlide(v));
+        }
+        else {
+            StartCoroutine(Slide(v));
+        }
+        
     }
 
-    IEnumerator Slide(float v)
+    IEnumerator FadeSlide(float v)
     {
         float elapsedTime=0;
-        float initialValue = GetComponent<Slider>().value;
+        float initialValue = sl.value;
+        Color fillColor = new Color(1, 1, 1, 0);
+        sl.fillRect.GetComponent<Image>().color = fillColor;
+        sl.handleRect.GetComponent<Image>().color = fillColor;
         
+        while (fillColor.a <= 0.5f) {
+            fillColor.a += 0.02f;
+            sl.fillRect.GetComponent<Image>().color = fillColor;
+            sl.handleRect.GetComponent<Image>().color = fillColor;
+            yield return new WaitForEndOfFrame();
+        }
+
         while(elapsedTime<speed)
         {
-            GetComponent<Slider>().value = Mathf.Lerp(initialValue, v, elapsedTime/speed);
+            sl.value = Mathf.Lerp(initialValue, v, elapsedTime/speed);
             elapsedTime += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
-        
-        
-      //  Debug.Log("ACE");
-        
+
+        while (fillColor.a >= 0)
+        {
+            fillColor.a -= 0.02f;
+            sl.fillRect.GetComponent<Image>().color = fillColor;
+            sl.handleRect.GetComponent<Image>().color = fillColor;
+            yield return new WaitForEndOfFrame();
+        }
+
     }
 
-     void Update()
+
+    IEnumerator Slide(float v)
+    {
+        float elapsedTime = 0;
+        float initialValue = sl.value;
+        while (elapsedTime < speed)
+        {
+            sl.value = Mathf.Lerp(initialValue, v, elapsedTime / speed);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+
+    void Update()
     {
         
     }
