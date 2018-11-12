f4878d06fa7f9d5764f9a833ea41d3773e925acf
rroyermoraes <rroyermoraes@outlook.com>
2018-11-12 00:37:05 -0200

    TestesNovoDesafio1

---
+++ TCC_Mark_Roger/Assets/ButtonAutoStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class ButtonAutoStart : MonoBehaviour {
+	void Start () {
+                ExecuteEvents.Execute(GetComponent<Button>().gameObject, new BaseEventData(FindObjectOfType<EventSystem>()), ExecuteEvents.submitHandler);
+    }
+}
